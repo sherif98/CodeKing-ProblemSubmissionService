3614600768039b5659f4f489c768a310b3ed419a
Sherif Hamdy <sherif.hamdy.1995@gmail.com>
2018-04-13 21:18:42 +0200

    eureka configs and mongodb configs

---
+++ src/test/java/com/codeking/problemsubmissionservice/ProblemsubmissionserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeking.problemsubmissionservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProblemsubmissionserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
